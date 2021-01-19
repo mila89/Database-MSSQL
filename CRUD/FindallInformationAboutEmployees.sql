dd329fc9f1af849072d9eccf90bc76359fc8d0a8
Milena Alexieva <doycheva_m@abv.bg>
2021-01-19 20:48:26 +0200

    Add files via upload

---
+++ CRUD/FindallInformationAboutEmployees.sql
@@ -0,0 +1,5 @@
+SELECT EmployeeID AS [ID], FirstName AS [First Name], LastName AS [Last Name],
+MiddleName as [Middle Name], JobTitle as [Job Title], DepartmentID as [Dept ID],
+ManagerID as [Mangr ID], HireDate as [Hire Date], Salary, AddressID FROM Employees 
+WHERE JobTitle = 'Sales Representative';
+
