f583283a248196bbf84ebe20fdf38bffc528eb81
Milena Alexieva <doycheva_m@abv.bg>
2021-01-23 21:15:38 +0200

    Add files via upload

---
+++ TableReleations/PeaksinRila .sql
@@ -0,0 +1,9 @@
+SELECT * FROM Peaks;
+
+SELECT * FROM Mountains;
+
+SELECT m.MountainRange, p.PeakName, p.Elevation
+FROM Mountains AS m
+JOIN Peaks As p ON p.MountainId=m.Id
+WHERE  m.MountainRange='Rila'
+ORDER BY P.Elevation DESC;
