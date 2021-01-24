7032d3778f05900894ea4a21da5debe23f063439
Milena Alexieva <doycheva_m@abv.bg>
2021-01-24 15:26:39 +0200

    Add files via upload

---
+++ TableReleations/Many-To-Many Relationship .sql
@@ -0,0 +1,33 @@
+CREATE TABLE Students (
+StudentID INT PRIMARY KEY IDENTITY,
+Name VARCHAR(20)
+)
+
+CREATE TABLE Exams (
+ExamID INT PRIMARY KEY IDENTITY(101,1),
+Name VARCHAR(20)
+)
+
+CREATE TABLE StudentsExams (
+StudentID INT FOREIGN KEY REFERENCES Students(StudentID),
+ExamID INT FOREIGN KEY REFERENCES Exams(ExamID),
+CONSTRAINT PK_StudentsExam PRIMARY KEY(StudentID,ExamID)
+)
+
+INSERT INTO Students(Name) VALUES
+('Mila'),                                      
+('Toni'),
+('Ron')
+
+INSERT INTO Exams(Name) VALUES
+('SpringMVC'),
+('Neo4j'),
+('Oracle 11g')
+
+INSERT INTO StudentsExams(StudentID,ExamID) VALUES
+(1,101),
+(1,102),
+(2,101),
+(3,103),
+(2,102),
+(2,103)
