23c19e772eeb72a4ac72d92abf6d0498602616cd
Milena Alexieva <doycheva_m@abv.bg>
2021-02-07 21:24:54 +0200

    Add files via upload

---
+++ TriggersAndTransactions/3.DepositMoney.sql
@@ -0,0 +1,25 @@
+CREATE PROC usp_DepositMoney (@AccountId INT, @MoneyAmount DECIMAL(15,4))
+AS
+	BEGIN TRANSACTION
+	DECLARE @ACCOUNT INT=(SELECT ID FROM Accounts WHERE ID=@AccountId)
+	if @Account is null
+	begin
+		ROLLBACK;
+		RAISERROR('Invalid account id',16,1)
+		RETURN
+	end
+	IF @MoneyAmount<0
+	BEGIN
+		ROLLBACK;
+		RAISERROR('The amount shoud be possitive!',16,1)
+		RETURN
+	END
+	UPDATE Accounts
+	SET Balance+=@MoneyAmount
+	WHERE Id=@Account
+	COMMIT
+GO
+
+EXEC usp_DepositMoney 1,120
+SELECT * FROM Accounts
+SELECT * FROM LOGS
