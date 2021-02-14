4683f2841c6da32d7547cf3d9f3c3081c179380f
Milena Alexieva <doycheva_m@abv.bg>
2021-02-14 21:46:30 +0200

    Add files via upload

---
+++ Exam_280620/2.Insert.sql
@@ -0,0 +1,14 @@
+INSERT Planets(Name) values
+('Mars'),
+('Earth'),
+('Jupiter'),
+('Saturn')
+
+INSERT Spaceships(Name,	Manufacturer,LightSpeedRate)
+VALUES
+('Golf','VW',3),
+('WakaWaka','Wakanda',4),
+('Falcon9','SpaceX',1),
+('Bed','Vidolov',6)
+
+
