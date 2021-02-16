0217b84ff6a337aa65283b0a8dc2c9e2a2798a9c
Milena Alexieva <doycheva_m@abv.bg>
2021-02-16 14:44:38 +0200

    Add files via upload

---
+++ Exam_13February_2021/4.sql
@@ -0,0 +1,10 @@
+DELETE Files WHERE CommitId=36
+
+DELETE COMMITS
+WHERE RepositoryId=3
+
+DELETE ISSUES WHERE RepositoryId=3
+
+DELETE RepositoriesContributors WHERE RepositoryId=3
+
+DELETE Repositories WHERE ID=3
